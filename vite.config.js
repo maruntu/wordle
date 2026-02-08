3d0c98c696010e8fc4e629935765019f0e540a36
maruntu <69143615+maruntu@users.noreply.github.com>
2026-02-08 14:13:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { version } from "./package.json";
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wordle/",
-  plugins: [svelte({
-    preprocess: vitePreprocess()
-  })],
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: `[name]-v${version}.[ext]`,
-        entryFileNames: `[name]-v${version}.js`,
-        dir: "./dist",
-      }
-    }
-  }
-});
+  base: '/wordle/',
+  plugins: [svelte()]
+})
